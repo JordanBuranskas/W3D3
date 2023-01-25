9b9612a915af8f47fdb102c66a87eb7050fd8be3
Jordan Buranskas <jordanburanskas@gmail.com>
2023-01-25 14:30:02 -0500

    test

--- recursion_proj.rb
+++ recursion_proj.rb
@@ -1,1 +1,22 @@
-"testing"
+def range (start, ending) #1, 7
+    return [] if ending < start
+    return [] if start == ending
+   
+
+    new_array = [start] << range(start +1, ending)
+    # range(10, 14) # [10, 11, 12, 13]
+
+    # [10] << range(11, 14)
+    #[10, 11] << range(12, 14)
+    #[10, 11, 12] << range(13, 14)
+    #[10, 11, 12, 13, []] << range(14, 14)
+    #[10, 11, 12, 13, []]
+
+    new_array.flatten
+
+end
+
+p range(10, 14)
+p range(-1, 4)
+p range(1, 3)
+p range(9, 9)
