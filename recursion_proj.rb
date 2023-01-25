8fb1b2f700f05e773a21954e5f8bcdbc398b21f2
Justin <jcdiner@gmail.com>
2023-01-25 14:50:49 -0500

    working on expo

--- recursion_proj.rb
+++ recursion_proj.rb
@@ -20,3 +20,26 @@ p range(10, 14)
 p range(-1, 4)
 p range(1, 3)
 p range(9, 9)
+
+def expo(b, n)
+	#base cases
+	return 1 if n == 0
+	return 0 if b == 0
+	return b if n == 1
+
+	b * expo(b, n-1)
+end
+
+p expo(2, 4)
+p expo(3, 3)
+
+def expo_2(b, n)
+	return 1 if n == 0
+	return 0 if b == 0
+	return b if n == 2 || 2
+	
+	if n.even? 
+
+	end
+end
+
